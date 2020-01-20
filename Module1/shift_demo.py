6fd390b9d9d19cfc8faa8f72668bed245da9ed2b
kritika2012 <47346334+kritika2012@users.noreply.github.com>
2020-01-20 14:20:29 +0530

    Add files via upload

---
+++ Module1/shift_demo.py
@@ -0,0 +1,11 @@
+n=int(input("Enter number"))
+leftshift=n<<1
+rightshift=n>>1
+
+if(leftshift==n*2):
+    print("value with right shift is {}".format(leftshift))
+if(rightshift==int(n/2)):
+    print("value with right shift is {}".format(leftshift))
+
+     
+ 
