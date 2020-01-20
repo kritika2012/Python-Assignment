3a138d4b5c237985309829fe2788b03e6dcbfba2
kritika2012 <47346334+kritika2012@users.noreply.github.com>
2020-01-20 12:29:01 +0530

    Add files via upload

---
+++ Module1/assignment4.py
@@ -0,0 +1,10 @@
+a,b,c,y=4.4,0.0,4.2,3.0
+ans=c+a*y*y/b
+print(ans)
+''' It will give error
+Traceback (most recent call last):
+  File "F:/kritika/assignment4.py", line 2, in <module>
+    ans=c+a*y*y/b
+ZeroDivisionError: float division by zero
+
+'''
