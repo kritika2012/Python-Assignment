2499515fdfd06f25b0161719e88345d4ff8cb27d
Kritika Bhatore <47346334+kritika2012@users.noreply.github.com>
2020-01-23 16:43:46 +0530

    Delete sortword.py

--- Module4/sortword.py
+++
@@ -1,9 +0,0 @@
-st=input("enter the input here ")
-st=st.split(" ")
-st.sort()
-lst=[]
-for i in range(len(st)):
-    if st[i] not in lst:
-        lst.append(st[i])
-for j in lst:
-    print(j,end=" ")
