a2dc8975e7f7189993700147520cac075f364c17
Kritika Bhatore <47346334+kritika2012@users.noreply.github.com>
2020-01-25 20:37:21 +0530

    Update gamestart.py

--- Module9/gamestart.py
+++ Module9/gamestart.py
@@ -1,3 +1,5 @@
+#This file imports the Deck_player module as dp
+#which then continues the game by calling further methods
 import Deck_player as dp
 nm=input('players enter Your name ')
 obj=dp.Player(nm)
